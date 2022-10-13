8a45c1f7bfc5eff19db61b423de0daaf0bd27e79
tolkien111 <tolkien111@wp.pl>
2022-10-13 14:02:47 +0200

    project initialized

---
+++ src/main/java/pl/sda/customersafterkurs/CustomersafterkursApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.customersafterkurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersafterkursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersafterkursApplication.class, args);
+	}
+
+}
