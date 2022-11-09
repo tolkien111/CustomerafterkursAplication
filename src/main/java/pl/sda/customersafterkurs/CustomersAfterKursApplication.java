26e343975ae1a846d3738308491f5acb6eba778f
tolkien111 <tolkien111@wp.pl>
2022-11-09 13:22:48 +0100

    Prepare test for CustomerAddressService with Mockito.

--- src/main/java/pl/sda/customersafterkurs/CustomersAfterKursApplication.java
+++ src/main/java/pl/sda/customersafterkurs/CustomersAfterKursApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomersafterkursApplication {
+public class CustomersAfterKursApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CustomersafterkursApplication.class, args);
+		SpringApplication.run(CustomersAfterKursApplication.class, args);
 	}
 
 }
---
+++ src/test/java/pl/sda/customersafterkurs/service/CustomerAddressServiceTest.java
@@ -0,0 +1,60 @@
+package pl.sda.customersafterkurs.service;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.stubbing.OngoingStubbing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import pl.sda.customersafterkurs.entity.Address;
+import pl.sda.customersafterkurs.entity.CustomerRepository;
+import pl.sda.customersafterkurs.entity.Person;
+import pl.sda.customersafterkurs.service.dto.AddAddressForm;
+import pl.sda.customersafterkurs.service.dto.CreatedAddress;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyDouble;
+
+@SpringBootTest
+@Transactional
+class CustomerAddressServiceTest {
+
+    @Autowired
+    private CustomerAddressService service;
+
+    @Autowired
+    private CustomerRepository repository;
+
+    @MockBean
+    private ReverseGeocoding reverseGeocoding;
+
+    @Test
+    void shouldAddAddressToCustomer() {
+        //Given
+        final var person = new Person("abs@wp.pl", "Jan", "Nowak", "99021211987");
+        repository.save(person);
+        var address = new Address("str", "Wawa", "01-200", "PL");
+        Mockito.when(reverseGeocoding.reverse(anyDouble(), anyDouble()))
+                .thenReturn(address);
+
+        final var form = new AddAddressForm(person.getId(), 52.242839, 20.9795852); //dzielna 78 warszawa
+
+
+        //When
+        final var createdAddress = service.addAddress(form);
+
+        //Then
+        assertNotNull(createdAddress);
+        assertEquals(new CreatedAddress(
+                person.getId(),
+                address.getId(),
+                "str",
+                "Wawa",
+                "01-200",
+                "PL"), createdAddress);
+    }
+
+
+}
