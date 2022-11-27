0d5cd313dc94a070b5b5470c801399f45cbb3d08
tolkien111 <tolkien111@wp.pl>
2022-11-27 16:39:20 +0100

    Develop and Refactoring GoogleReverseGeocoding and new Exception - InvalidCoordinatesException

--- src/main/java/pl/sda/customersafterkurs/service/GoogleReverseGeocoding.java
+++ src/main/java/pl/sda/customersafterkurs/service/GoogleReverseGeocoding.java
@@ -2,14 +2,25 @@ package pl.sda.customersafterkurs.service;
 
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.AddressComponent;
+import com.google.maps.model.AddressComponentType;
 import com.google.maps.model.LatLng;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import pl.sda.customersafterkurs.entity.Address;
+import pl.sda.customersafterkurs.service.exception.InvalidCoordinatesException;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 import static com.google.maps.model.AddressComponentType.*;
 import static java.lang.String.format;
 import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
 
 @Component
 @RequiredArgsConstructor
@@ -25,38 +36,69 @@ public class GoogleReverseGeocoding implements ReverseGeocoding {
             final var results = GeocodingApi
                     .reverseGeocode(context, new LatLng(latitude, longitude)) // tworzy request
                     .await();//stworzony request zostaje wyslany
+
             if (results.length == 0)
-                throw new ReverseGeoCodingException(format("invalid lat: %s long: %s long", latitude, longitude));
+                throw new InvalidCoordinatesException(format("invalid lat: %s long: %s long", latitude, longitude));
 
-            final var result = results[0];
+            final var addressComponents = results[0].addressComponents;
 
-            String streetNumber = null;
-            String street = null;
-            String city = null;
-            String zipCode = null;
-            String countryCode = null;
+            final var streetNumber = findValue(addressComponents, List.of(STREET_NUMBER, PREMISE));
+            final var street = findOptionalValue(addressComponents, ROUTE);
+            final var city = findValue(addressComponents, LOCALITY);
+            final var zipCode = findValue(addressComponents, POSTAL_CODE);
+            final var countryCode = findValue(addressComponents, COUNTRY);
 
-            for (final var component : result.addressComponents) {
-                final var types = asList(component.types);
-                if (types.contains(STREET_NUMBER)) {
-                    streetNumber = component.shortName;
-                } else if (types.contains(ROUTE)) {
-                    street = component.shortName;
-                } else if (types.contains(LOCALITY)) {
-                    city = component.shortName;
-                } else if (types.contains(POSTAL_CODE)){
-                    zipCode = component.shortName;
-                } else if (types.contains(COUNTRY)){
-                    countryCode = component.shortName;
-                }
-            }
+//            for (final var component : result.addressComponents) { //mniej przebiegów, bardziej wydajne, ale w naszym przypadku nieistotne ponieważ iterujemy po KRÓTKIEJ TABLICY
+//                final var types = asList(component.types);
+//                if (types.contains(STREET_NUMBER)) {
+//                    streetNumber = component.shortName;
+//                } else if (types.contains(ROUTE)) {
+//                    street = component.shortName;
+//                } else if (types.contains(LOCALITY)) {
+//                    city = component.shortName;
+//                } else if (types.contains(POSTAL_CODE)){
+//                    zipCode = component.shortName;
+//                } else if (types.contains(COUNTRY)){
+//                    countryCode = component.shortName;
+//                }
+//            }
 
 
-            return new Address(street + " " + streetNumber, city, zipCode, countryCode);
-
-        } catch (Exception ex) {
+            return new Address(
+                    street != null ? street + " " + streetNumber : streetNumber, //jeżeli street różne od null to street łącz z streetNumber w przeciwnym wypadku użyj tylko streetNumber
+                    city,
+                    zipCode,
+                    countryCode);
+        } catch (ApiException | InterruptedException | IOException ex) {
             throw new ReverseGeoCodingException("google ist failing ", ex);
         }
+    }
+
+    private String findValue(AddressComponent [] components, AddressComponentType type) {
+        for (final var component : components) {
+            if (asList(component.types).contains(type)) {
+                return component.shortName;
+            }
+        }
+        throw new InvalidCoordinatesException("cannnot find address part: " + type);
+    }
+
+    private String findValue(AddressComponent [] components, Collection<AddressComponentType> types) { // sposób na szukanie elementu z listy w liście
+        for (final var component : components) {
+            if (stream(component.types).anyMatch(types::contains)) { //asList(component.types).stream().anyMatch(type ->types.contains(type))
+                return component.shortName;
+            }
+        }
+        throw new InvalidCoordinatesException("cannnot find address part: " + types);
+    }
+
+    private String findOptionalValue (AddressComponent [] components, AddressComponentType type){
+        for (final var component : components) {
+            if (asList(component.types).contains(type)) {
+                return component.shortName;
+            }
+        }
+        return null;
     }
 
 }
---
+++ src/main/java/pl/sda/customersafterkurs/service/exception/InvalidCoordinatesException.java
@@ -0,0 +1,8 @@
+package pl.sda.customersafterkurs.service.exception;
+
+public class InvalidCoordinatesException extends BusinessServiceException {
+
+    public InvalidCoordinatesException(String message) {
+        super(message);
+    }
+}
