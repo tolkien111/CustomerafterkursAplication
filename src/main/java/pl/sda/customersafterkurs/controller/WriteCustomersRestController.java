a5588868ec8fa72b5cb4ae671afeb495a8755a9d
tolkien111 <tolkien111@wp.pl>
2022-12-14 14:20:17 +0100

    1. Add addAddress method for customer and inject CustomerAddressService to use in addAddress method. 2. Static class only for addAddress method 3. Refactoring name of CustomerRegistrationService

--- src/main/java/pl/sda/customersafterkurs/controller/WriteCustomersRestController.java
+++ src/main/java/pl/sda/customersafterkurs/controller/WriteCustomersRestController.java
@@ -2,14 +2,15 @@ package pl.sda.customersafterkurs.controller;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.function.EntityResponse;
+import pl.sda.customersafterkurs.service.CustomerAddressService;
 import pl.sda.customersafterkurs.service.CustomerRegistrationService;
-import pl.sda.customersafterkurs.service.dto.RegisterCompanyForm;
-import pl.sda.customersafterkurs.service.dto.RegisterPersonForm;
-import pl.sda.customersafterkurs.service.dto.RegisteredCustomerId;
+import pl.sda.customersafterkurs.service.dto.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
@@ -17,20 +18,40 @@ import pl.sda.customersafterkurs.service.dto.RegisteredCustomerId;
 public class WriteCustomersRestController {
 
     @NonNull
-    private final CustomerRegistrationService service;
+    private final CustomerRegistrationService registrationService;
+
+    @NonNull
+    private final CustomerAddressService addressService;
 
     @PostMapping("/companies")
     ResponseEntity<RegisteredCustomerId> registerCompany(@RequestBody RegisterCompanyForm form){
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(service.registerCompany(form));
+                .body(registrationService.registerCompany(form));
 
     }
     @PostMapping("/people")
     ResponseEntity<RegisteredCustomerId> registerPerson(@RequestBody RegisterPersonForm form){
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(service.registerPerson(form));
+                .body(registrationService.registerPerson(form));
+    }
+
+    @Value
+    static class LatLong{
+
+        double latitude;
+        double longitude;
+    }
+
+    @PostMapping("/customers/{customerId}/addresses")
+    ResponseEntity<CreatedAddress> addAddress(@PathVariable("customerId")  UUID id,
+                                              @RequestBody LatLong latLong){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(addressService.addAddress(new AddAddressForm(id,
+                        latLong.getLatitude(),
+                        latLong.getLongitude())));
     }
 
 }
